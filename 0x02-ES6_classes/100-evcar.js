bfa6a059811a1654bdafaace0b1d9a07a1196fc4
ali-mahmoud98 <ali98mahmoudr@gmail.com>
2024-07-26 22:34:06 +0300

    .

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,4 +1,4 @@
-import Car from './10-car.js';
+import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
@@ -7,6 +7,6 @@ export default class EVCar extends Car {
   }
 
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
+    return new Car(this._brand, this._motor, this._color).cloneCar();
   }
 }
