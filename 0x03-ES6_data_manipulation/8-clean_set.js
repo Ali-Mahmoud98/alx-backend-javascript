cc896dc255fb00eff656186ab90a6b2b5b2f5066
ali-mahmoud98 <ali98mahmoudr@gmail.com>
2024-08-09 12:23:23 +0300

    returns a string of all the set values that start with a specific string (startString).

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,27 @@
-export default function cleanSet(xSet, xStartString) {
+// export default function cleanSet(xSet, xStartString) {
+//   if (!xSet || !xStartString || !(xSet instanceof Set) || typeof xStartString !== 'string') {
+//     return '';
+//   }
+//   return Array.from(xSet)
+//     .filter((ele) => ele && ele.startsWith(xStartString))
+//     .map((ele) => ele.replace(xStartString, ''))
+//     .join('-');
+// }
+
+function cleanSet(xSet, xStartString) {
   if (!xSet || !xStartString || !(xSet instanceof Set) || typeof xStartString !== 'string') {
     return '';
   }
-  return Array.from(xSet)
-    .filter((ele) => ele && ele.startsWith(xStartString))
-    .map((ele) => ele.replace(xStartString, ''))
-    .join('-');
+
+  const result = [];
+
+  for (const value of xSet) {
+    if (value.startsWith(xStartString)) {
+      result.push(value.slice(xStartString.length));
+    }
+  }
+
+  return result.join('-');
 }
+
+export default cleanSet;
